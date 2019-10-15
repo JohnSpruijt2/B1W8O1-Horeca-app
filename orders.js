601afe6cffb184a0d3b73c7a668dbaf0b617a797
John Spruijt <99058314@mydavinci.nl>
2019-10-15 14:20:41 +0200

    prices | fixed decimals | bit of style | var into const

--- orders.js
+++ orders.js
@@ -1,30 +1,39 @@
+const frisPrice = 1.99                                                                                                //Prices
+const bierPrice = 2.49
+const wijnPrice = 3.49
+const bitterballen2Price = 4.99
+const bitterballen3Price = 8.99
+
 var conti = true
-var fris
-var bier
-var wijn
+var fris = 0
+var bier = 0
+var wijn = 0
+var bitterballen1 = 0
+var bitterballen2 = 0
+var bitterballen3 = 0
 alert("Welkom bij Bar Barratata! \n\nU gaat nu bestellen.")
 while (conti == true) {
     var order = prompt("Welke bestelling wilt u toevoegen?")
-    if (order == "stop") {
+    if (order == "stop") {                                                                                          //Stop function
         break;
     }
     else if (order == "snack"){                                                                                     //Als je 'snack' invult dan gaat het over in bitterball
-        var bitterballen1 = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?")                              //Grote van de schaal van de bitterballen
+        bitterballen1 = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?")                              //Grote van de schaal van de bitterballen
         if (bitterballen1 == 8) {
-            var bitterballen2 = prompt("Hoeveel bitterbalschalen van " + bitterballen1 + " stuks wilt u bestellen?")
+            bitterballen2 = prompt("Hoeveel bitterbalschalen van " + bitterballen1 + " stuks wilt u bestellen?")//Hoeveelheid van bitterballen schaal 8
             if (bitterballen2 == "stop") {
                 break;
             }
             document.write(bitterballen2)
         }
         else if (bitterballen1 == 16) {
-            var bitterballen3 = prompt("Hoeveel bitterbalschalen van " + bitterballen1 + " stuks wilt u bestellen?")
+            bitterballen3 = prompt("Hoeveel bitterbalschalen van " + bitterballen1 + " stuks wilt u bestellen?")//Hoeveelheid van bitterballen schaal 16
             if (bitterballen3 == "stop") {
                 break;
             }
             document.write(bitterballen3)
         }
-        else if (bitterballen1 == "stop") {
+        else if (bitterballen1 == "stop") {                                                                         //Stop function
             break;
         }
         else {
@@ -39,15 +48,12 @@ while (conti == true) {
         }
         if (order == "fris") {
             fris = number
-            document.write(fris)
         }
         else if (order == "bier") {
             bier = number
-            document.write(bier)
         }
         else if (order == "wijn") {
             wijn = number
-            document.write(wijn)
         }
         else {
             alert("U heeft een ongeldige invoer gedaan. Uw bestelling kan niet worden toegevoegd.")
@@ -58,3 +64,26 @@ while (conti == true) {
     }
 }
 
+if (wijn >0) {
+    document.write("<a class='billing'>Wijn | "+ wijn+" x € " + wijnPrice + "</a><br>")
+}
+if (bier >0 ) {
+    document.write("<a class='billing'>Bier | " + bier + " x € " + bierPrice + "</a><br>")
+}
+if (fris >0) {
+    document.write("<a class='billing'>Fris | " + fris + " x € " + frisPrice + "</a><br>")
+}
+if (bitterballen2 >0) {
+    document.write("<a class='billing'>Bitterballen (8) | " + bitterballen2 + " x € " + bitterballen2Price + "</a><br>")
+}
+if (bitterballen3 >0) {
+    document.write("<a class='billing'>Bitterballen (16) | " + bitterballen3 + " x  " + bitterballen3Price + "</a><br>")
+}
+var wijnTotal = wijn*wijnPrice
+var bierTotal = bier*bierPrice
+var frisTotal = fris*frisPrice
+var bitterballen2Total = bitterballen2*bitterballen2Price
+var bitterballen3Total = bitterballen3*bitterballen3Price
+var totalPrice = (wijnTotal+bierTotal+frisTotal+bitterballen2Total+bitterballen3Total).toFixed(2);
+document.write("<h2 id='total'>Totaal</h2>")
+document.write("<a id='totalPrice'>€ " + totalPrice)
