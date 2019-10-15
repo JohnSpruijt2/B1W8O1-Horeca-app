e9a06104aeac55f29ae9af5e136f2b03c3524946
John Spruijt <99058314@mydavinci.nl>
2019-10-15 10:59:53 +0200

    bitterballen upgraded

--- orders.js
+++ orders.js
@@ -1,17 +1,31 @@
 var conti = true
-var fris = 0
-var bier = 0
-var wijn = 0
+var fris
+var bier
+var wijn
 alert("Welkom bij Bar Barratata! \n\nU gaat nu bestellen.")
 while (conti == true) {
     var order = prompt("Welke bestelling wilt u toevoegen?")
     if (order == "stop") {
         break;
     }
-    else if (order == "snack"){
-        var bitterballen1 = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?")
-        if (bitterballen1 == 8,16) {
+    else if (order == "snack"){                                                                                     //Als je 'snack' invult dan gaat het over in bitterball
+        var bitterballen1 = prompt("Hoeveel bitterballen wilt u toevoegen (8 of 16)?")                              //Grote van de schaal van de bitterballen
+        if (bitterballen1 == 8) {
             var bitterballen2 = prompt("Hoeveel bitterbalschalen van " + bitterballen1 + " stuks wilt u bestellen?")
+            if (bitterballen2 == "stop") {
+                break;
+            }
+            document.write(bitterballen2)
+        }
+        else if (bitterballen1 == 16) {
+            var bitterballen3 = prompt("Hoeveel bitterbalschalen van " + bitterballen1 + " stuks wilt u bestellen?")
+            if (bitterballen3 == "stop") {
+                break;
+            }
+            document.write(bitterballen3)
+        }
+        else if (bitterballen1 == "stop") {
+            break;
         }
         else {
             alert("U kunt alleen een keuze maken tussen 8 en 16. De snacks zijn niet toegevoegd aan de bestelling.")
